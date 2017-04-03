c6c988a43f54bcf37c153036d323019559e20cc4
Monty A <monty@dotnetpro.co.uk>
2017-04-03 10:23:20 +0100

    Working code.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # RunTSCIgnoreErrors
-Runs TSC and ignores errors.
+Runs TSC and ignores errors, and exits with code 0.
+
+# Why ?
+
+If you have to ask, it is not for you.
+
+# Are you serious?
+
+Yes.
---
+++ RunTSCIgnoreErrors/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunTSCIgnoreErrors
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                return;
+            }
+            string tsConfigPath = args[0];
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.UseShellExecute = true;
+            info.CreateNoWindow = true;
+            info.FileName = "tsc";
+            info.Arguments = $"--project \"{tsConfigPath}\"";
+            var process = Process.Start(info);
+            process.WaitForExit();
+            Environment.Exit(0);
+        }
+    }
+}
